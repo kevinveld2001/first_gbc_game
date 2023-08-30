b37a0b4f8f0a935a40de0e3867ae3cefdc2484c8
Kevin V <517038@student.saxion.nl>
2023-08-30 11:48:25 +0200

    init

---
+++ make.bat
@@ -0,0 +1,3 @@
+c:\gbdk\bin\lcc -Wa-l -Wl-m -Wl-j -DUSE_SFR_FOR_REG -c -o out/main.o src/main.c
+c:\gbdk\bin\lcc -Wa-l -Wl-m -Wl-j -DUSE_SFR_FOR_REG -o out/main.gb out/main.o
+c:\gb\bgb\bgb.exe ./out/main.gb
