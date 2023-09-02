c4b80d3aaada1bb18ae2684eccd7e8357e2e7028
Kevin V <517038@student.saxion.nl>
2023-09-02 12:29:05 +0200

    remove after build using bat

--- make.bat
+++ make.bat
@@ -13,6 +13,21 @@ for /r %SOURCE_PATH% %%i in (*.c) do (
 
 %GBDK_PATH%\lcc -Wa-l -Wl-m -Wl-j -DUSE_SFR_FOR_REG -Wl-yp0x143=0x80 -o %OUTPUT_PATH%\main.gbc %OUTPUT_PATH%\*.o
 
+//del out dir exept main.gbc
+DEL %OUTPUT_PATH%\*.o
+DEL %OUTPUT_PATH%\*.lst
+DEL %OUTPUT_PATH%\*.sym
+DEL %OUTPUT_PATH%\*.asm
+DEL %OUTPUT_PATH%\*.cdb
+DEL %OUTPUT_PATH%\*.ihx
+DEL %OUTPUT_PATH%\*.map
+DEL %OUTPUT_PATH%\*.noi
+
+//remove from main dir
+DEL *.cdb
+DEL *.ihx
+DEL *.map
+DEL *.noi
 
 echo "start game..."
 %BGB_PATH%\bgb.exe ./out/main.gbc
